d2f0ebc43660bb6eca521e3566378b8d3577b239
Dibya <Dibya.DebayanDash@siriuscom.com>
2022-09-06 21:35:09 +0530

    Changed the expalnation

--- Week-3/3.3/index.js
+++ Week-3/3.3/index.js
@@ -15,3 +15,4 @@ increment();
 increment();
 log(); // Output : Count is 0
 //This is because the scope of the count declared in line number 6 is the function createIncrement. Hence it outputs as 0
+// log function is creating closure with createIncreament function and the closure contains message variable with initialised value
